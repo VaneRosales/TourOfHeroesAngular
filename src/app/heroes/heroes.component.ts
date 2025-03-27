c9fdf0fb0701a68ea8b734e69cda148e487cb3aa
VaneRosales <jvanessa.rr@gmail.com>
2025-03-27 14:25:11 -0600

    Mostrar una lista de seleccon

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,17 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Hero } from '../hero';
 import { FormsModule } from '@angular/forms';
+import { HEROES } from '../../mock-heroes';
+import { NgIf, NgFor, UpperCasePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule,FormsModule,UpperCasePipe, NgFor, NgIf],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes= HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
