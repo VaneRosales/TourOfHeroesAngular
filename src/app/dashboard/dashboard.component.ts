627cff1d70b1084c1ec232881c31f265b43f3de5
VaneRosales <jvanessa.rr@gmail.com>
2025-03-31 15:13:44 -0600

    datos de un servidor

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
+import { HeroSearchComponent } from "../hero-search/hero-search.component";
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, HeroSearchComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
+  //Inyeccion de dependencias en lugar de tener el constructor
+  //heroService=inject (HeroService);
 
   constructor(private heroService: HeroService) {}
 
